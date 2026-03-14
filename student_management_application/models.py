716505172951e582fcb0c07c2a295588a541584a
Sayon Camara <sayon.camara@ubagroup.com>
2026-03-14 12:51:48 +0000

    update model

--- student_management_application/models.py
+++ student_management_application/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.name
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    # relation ManyToMany avec Student via Score
+    students = models.ManyToManyField(Student, through='Score')
+
+    def __str__(self):
+        return self.name
+
+
+class Score(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student} - {self.course} : {self.score}"
