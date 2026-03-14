670f229c0519674a3da7a1a5b7ae626cd94cf751
Sayon Camara <sayon.camara@ubagroup.com>
2026-03-14 12:28:36 +0000

    update forms

--- student_management_application/forms.py
+++ student_management_application/forms.py
@@ -1,23 +1,54 @@
 # forms.py
 
 from django import forms
+from django.shortcuts import redirect, render
 from .models import Student, Course, Score
+
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
+        fields = ['name', 'age', 'sex']
+
     def add_student(request):
-        student = super().save(commit=False)
-        if request: 
-            student.save()
-        return student
+        if request.method == "POST":
+            form = StudentForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('student_list')
+        else:
+            form = StudentForm()
+
+        return render(request, 'add_student.html', {'form': form})
     
+    def student_list(request):
+        students = Student.objects.all()
+        return render(request, 'student_list.html', {'students': students})
+
+
 class CourseForm(forms.ModelForm):
     class Meta:
-        # Specifies the model to use for the form 
         model = Course
+        fields = ['name', 'teacher']
+
+    def course_list(request):
+        courses = Course.objects.all()
+        return render(request, 'course_list.html', {'courses': courses})
+
 
 class ScoreForm(forms.ModelForm):
     class Meta:
-        # Specifies the model to use for the form 
         model = Score
+        fields = ['student', 'course', 'score']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Personnalisation des widgets
+        self.fields['student'].widget.attrs.update({'class': 'form-control'})
+        self.fields['course'].widget.attrs.update({'class': 'form-control'})
+        self.fields['score'].widget.attrs.update({'class': 'form-control'})
+
+    def score_list(request):
+        scores = Score.objects.all()
+        return render(request, 'score_list.html', {'scores': scores})
